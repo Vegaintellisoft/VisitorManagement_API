8b5467f22c0f8cfe74110d0124de3d6cc76b1a2e
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-06-21 01:06:14 -0700

    minor fixes

--- queries/department_queries.js
+++ queries/department_queries.js
@@ -1,15 +1,15 @@
 const getAllDepartments = `
   SELECT d.*, c.name as company_name 
   FROM departments d 
-  JOIN companies c ON d.company_id = c.id
+  JOIN companies c ON d.company_id = c.company_id
   WHERE visibility=true;
 `;
 
 const getDepartmentByCompany = `
   SELECT d.*
   FROM departments d 
-  JOIN companies c ON d.company_id = c.id
-  WHERE c.id = ? AND visibility=true;
+  JOIN companies c ON d.company_id = c.company_id
+  WHERE c.company_id = ? AND visibility=true;
 `
 
 const getDepartmentById = `
@@ -17,11 +17,11 @@ const getDepartmentById = `
 `;
 
 const createDepartment = `
-  INSERT INTO departments (company_id, depart_name, status) VALUES (?, ?, ?);
+  INSERT INTO departments (company_id, dept_name, status) VALUES (?, ?, ?);
 `;
 
 const updateDepartment = `
-  UPDATE departments SET company_id = ?, name = ?, status = ? WHERE id = ?;
+  UPDATE departments SET company_id = ?, dept_name = ?, status = ? WHERE department_id = ?;
 `;
 
 const deleteDepartment = `
